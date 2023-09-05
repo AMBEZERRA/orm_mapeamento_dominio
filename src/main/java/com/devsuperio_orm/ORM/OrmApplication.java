d63ef183effa57d8f00e8b7416667af70986d50b
Allisson Bezerra <allisson.geografia@gmail.com>
2023-09-05 20:06:07 -0300

    Icicianndo orm desafio2 H2 e pom_xml config

---
+++ src/main/java/com/devsuperio_orm/ORM/OrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperio_orm.ORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperio_orm/ORM/OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperio_orm.ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
